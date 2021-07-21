2df720d1bfc2bfee0588cf180a883cdb9363b777
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2021-07-21 16:54:43 +0100

    Changed to keep sub-communities as a lot of inconsistency in whether main or sub-communities present or absent. Ditto with number of spp per sample. Does end up 860 (sub-)communties which will be a problem.

--- habitat_pseudo.R
+++ habitat_pseudo.R
@@ -1,25 +1,47 @@
 # Generate habitat-level pseudo quads
 library(tidyverse)
+rm(list=ls())
 
 all_nvc <- read_csv("data/NVC_Floristic_Table.csv")
 all_nvc <- all_nvc[, -1] # Do not need row number
 
-# Drop sub-communities
-communities_only <- !str_detect(all_nvc$`Community or sub-community name`, "sub-community")
-main_nvc <- all_nvc[communities_only,]
 
 # Extract out and remove rows containing spp numbers
-# NOTE: NEED TO FIX MG7 AND SM12
-spp_per_nvc <- filter(main_nvc, `Species name or special variable` == "Number of species per sample")
-spp_per_nvc <- spp_per_nvc[, -c(4,5,6)]
-colnames(spp_per_nvc)[4] <- "spp_per_sample"
-main_nvc <- filter(main_nvc, !`Species name or special variable` == "Number of species per sample")
-main_nvc <- filter(main_nvc, !`Species name or special variable` == "Vegetation height cm")
-main_nvc <- filter(main_nvc, !`Species name or special variable` == "Mean total cover")
-main_nvc <- filter(main_nvc, !is.na(`Maximum abundance of species`))
-main_nvc <- select(main_nvc, -`Special variable value`)
-main_nvc <- mutate(main_nvc, habitat = sub("^([[:alpha:]]*).*", "\\1", main_nvc$`Community level code`))
+# Several NVCs, e.g. MG7, do not have a 'main' community, only sub-communities
+# Some only have a main community, e.g. S3
+# Treat these the same as anything else for now
+# Not all communities have estimated numbers of spp per sample
+# None of OV communities have estimated numbers of spp per sample in spreadsheet
+# although they are present in handbooks. Do not use this information for now.
+# Whilst it may give more realistic pseudoquads, will such info be available
+# in international comparators? i.e. need to have as generalisable a method as
+# possible.
+# S4e and S4f do not have sub-community names in spreadsheet; Correct them here:
+all_nvc$`Community or sub-community name`[all_nvc$`Community or sub-community code` == "S4e"] <-
+  "Phragmites australis S4e" # Once book online add correct sub-community
+all_nvc$`Community or sub-community name`[all_nvc$`Community or sub-community code` == "S4f"] <-
+  "Phragmites australis S4f" # Once book online add correct sub-community
+
+# Cannot drop sub-communities given some lack main community.
+# Do not use spp_per_sample due to inconsistencies in database and lack of
+# generalisability to international classifications
+
+all_nvc <- filter(all_nvc, !`Species name or special variable` == "Number of species per sample")
+all_nvc <- filter(all_nvc, !`Species name or special variable` == "Vegetation height cm")
+all_nvc <- filter(all_nvc, !`Species name or special variable` == "Mean total cover")
+all_nvc <- filter(all_nvc, !is.na(`Maximum abundance of species`))
+all_nvc <- select(all_nvc, -`Special variable value`)
+all_nvc <- mutate(all_nvc, habitat = sub("^([[:alpha:]]*).*", "\\1",
+                                           all_nvc$`Community level code`))
+all_nvc <- mutate(all_nvc,
+                  const_prob = case_when(`Species constancy value` == "I" ~ 0.2,
+                                         `Species constancy value` == "II" ~ 0.4,
+                                         `Species constancy value` == "III" ~ 0.6,
+                                         `Species constancy value` == "IV" ~ 0.8,
+                                         `Species constancy value` == "V" ~ 1))
+
 
 # Create random main habitats; done at NVC main level but will be grouped at
 # habitat level for ordination purposes
-no_of_nvcs <- nrow(spp_per_nvc)
+no_of_nvcs <- length(unique(all_nvc$`Community or sub-community code`))
+no_of_habitats <- length(unique(all_nvc$habitat))
